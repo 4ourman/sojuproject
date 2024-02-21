e5ea1f8a059566988dcbcbe91ffeeca0b4337ea9
yirang <olrang.2@gmail.com>
2024-02-21 16:49:21 +0900

    first commit

---
+++ src/main/java/org/fourman/sojuproject/SojuprojectApplication.java
@@ -0,0 +1,13 @@
+package org.fourman.sojuproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SojuprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SojuprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fourman/sojuproject/SojuprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fourman.sojuproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SojuprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

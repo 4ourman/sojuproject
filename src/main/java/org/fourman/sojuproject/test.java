f3fe0fed5a7e8039a2dcb7f1464bb284030eeaab
이은재 <>
2024-02-22 11:00:35 +0900

    test

---
+++ src/main/java/org/fourman/sojuproject/test.java
@@ -0,0 +1,10 @@
+package org.fourman.sojuproject;
+
+public class test {
+    public  void a() {
+        int a = 10;
+        int b= 10;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}

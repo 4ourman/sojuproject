f65c74ec186fd7cd294e3c716f6cb16ff933f106
980pro <dnjswo8820@gmail.com>
2024-02-26 16:43:47 +0900

    category delete & update

--- src/main/java/org/fourman/sojuproject/reposittory/CategoryRepository.java
+++
@@ -1,6 +0,0 @@
-package org.fourman.sojuproject.reposittory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, String> {
-}
